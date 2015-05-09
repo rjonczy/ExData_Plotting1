aa4a2109383fc2b09d3e7aaaeb9b834098137edf
Robert Jonczy <robert.jonczy@omicrons.pl>
2015-05-09 16:37:18 +0200

    plot1.R implemented

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
-# load data
-power <- read.csv("household_power_consumption.txt", header = T, sep = ";", na.strings = "?", colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+### load data ###
+power <- read.csv("household_power_consumption.txt", header = T, sep = ";", na.strings = "?", stringsAsFactors=F, colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
 
 power$Time <- strptime(paste(power$Date, power$Time, sep=" "), format = "%d/%m/%Y %H:%M:%S")
 power$Date <- as.Date(power$Date, format = "%d/%m/%Y")
@@ -7,8 +7,13 @@ power$Date <- as.Date(power$Date, format = "%d/%m/%Y")
 # subset only observertions between 2007-02-01 and 2007-02-02
 power.s <- subset(power, Date >= "2007-02-01" & Date <= "2007-02-02")
 
+#release memory
+rm(power)
 
+### plot data ###
+hist(power.s$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 
-# plot data and save to plot1.png file
-
+#save to plot1.png file
+dev.copy(png, file = "plot1.png", width = 480, height = 480)
+dev.off()
 
